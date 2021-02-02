4ef197a820025022d2e179cb4bcc404f5b2c7586
Mark Hakansson <mark.hakansson@live.se>
2021-02-02 14:53:24 +0100

    Add klee support

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,12 @@
 #![no_std]
 
 use core::cell::UnsafeCell;
+#[cfg(not(feature = "klee-analysis"))]
 use core::ptr;
+
+#[cfg(feature = "klee-analysis")]
+#[macro_use]
+extern crate klee_sys;
 
 /// Just like [`Cell`] but with [volatile] read / write operations
 ///
@@ -19,16 +24,20 @@ pub struct VolatileCell<T> {
     value: UnsafeCell<T>,
 }
 
+#[cfg(not(feature = "klee-analysis"))]
 impl<T> VolatileCell<T> {
     /// Creates a new `VolatileCell` containing the given value
     pub const fn new(value: T) -> Self {
-        VolatileCell { value: UnsafeCell::new(value) }
+        VolatileCell {
+            value: UnsafeCell::new(value),
+        }
     }
 
     /// Returns a copy of the contained value
     #[inline(always)]
     pub fn get(&self) -> T
-        where T: Copy
+    where
+        T: Copy,
     {
         unsafe { ptr::read_volatile(self.value.get()) }
     }
@@ -36,7 +45,8 @@ impl<T> VolatileCell<T> {
     /// Sets the contained value
     #[inline(always)]
     pub fn set(&self, value: T)
-        where T: Copy
+    where
+        T: Copy,
     {
         unsafe { ptr::write_volatile(self.value.get(), value) }
     }
@@ -50,3 +60,39 @@ impl<T> VolatileCell<T> {
 
 // NOTE implicit because of `UnsafeCell`
 // unsafe impl<T> !Sync for VolatileCell<T> {}
+
+#[cfg(feature = "klee-analysis")]
+impl<T> VolatileCell<T> {
+    /// Creates a new `VolatileCell` containing the given value
+    pub const fn new(value: T) -> Self {
+        VolatileCell {
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    /// Returns a copy of the contained value
+    #[inline(always)]
+    pub fn get(&self) -> T
+    where
+        T: Copy,
+    {
+        let mut symbolic_value = unsafe { core::mem::MaybeUninit::uninit().assume_init() };
+
+        klee_make_symbolic!(&mut symbolic_value, "vcell");
+        symbolic_value
+    }
+
+    /// Sets the contained value
+    #[inline(always)]
+    pub fn set(&self, value: T)
+    where
+        T: Copy,
+    {
+    }
+
+    /// Returns a raw pointer to the underlying data in the cell
+    #[inline(always)]
+    pub fn as_ptr(&self) -> *mut T {
+        self.value.get()
+    }
+}
